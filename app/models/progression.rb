8ca45ec29156c6c054f652d9c19052d86f28d94f
MJ <mark.a@serviceseeking.com.au>
2016-09-06 01:00:32 +1000

    refactor next method
    * substitute algorithm to resemble domain more accurately. There is a pre-determined list/sequence of lessons. To progress to the next lesson, we can find where we are in the sequence and get the next one.

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -1,18 +1,21 @@
 class Progression < ActiveRecord::Base
   NUMBER_OF_LESSONS = 100
   PARTS_PER_LESSON = 3
+  LESSON_SEQUENCE = (1..NUMBER_OF_LESSONS).map{|i| (1..PARTS_PER_LESSON).map{|n| [i,n]}}.flatten(1)
 
   def next
     return if last_progression?
-    next_part_number = part_number + 1
-    next_lesson_number = lesson_number + (next_part_number > PARTS_PER_LESSON ? 1 : 0)
-    next_part_number = 1 if next_part_number > PARTS_PER_LESSON
+    next_lesson_number, next_part_number = LESSON_SEQUENCE[current_lesson_index + 1]
     Progression.new(lesson_number: next_lesson_number, part_number: next_part_number, student_id: student_id)
   end
 
   private
 
   def last_progression?
-    part_number == PARTS_PER_LESSON && lesson_number == NUMBER_OF_LESSONS
+    [lesson_number, part_number] == LESSON_SEQUENCE.last
+  end
+
+  def current_lesson_index
+    LESSON_SEQUENCE.index([lesson_number, part_number])
   end
 end
