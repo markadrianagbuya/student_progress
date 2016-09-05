a14bd351ff457a95628d0a99b3ae01da7a4b029c
MJ <mark.a@serviceseeking.com.au>
2016-09-05 22:26:29 +1000

    Finish required route functionality
    * The POST progressions route now persists a progression from the supplied params

--- app/controllers/progressions_controller.rb
+++ app/controllers/progressions_controller.rb
@@ -1,6 +1,7 @@
 class ProgressionsController < ApplicationController
 
   def create
+    Progression.create(params.permit(:student_id, :lesson_number, :part_number))
     render nothing: true
   end
 
--- spec/requests/student_progress_spec.rb
+++ spec/requests/student_progress_spec.rb
@@ -2,19 +2,20 @@ require "rails_helper"
 
 RSpec.describe "Student Progress", :type => :request do
   it "sets a student's progress" do
-    post "/progressions", progression: {student_id: 1, lesson_number: 1, part_number: 1}
+    Student.create(id: 1)
+    post "/progressions", {student_id: 1, lesson_number: 1, part_number: 1}
 
     get "/students/1"
     json_response = JSON.parse(response.body)
 
     expect(response.content_type).to eq("application/json")
     expect(response).to be_success
-    expect(json_response).to include("student_id" => 1, "lesson_number" => 1, "part_number" => 1)
+    expect(json_response).to include("id" => 1, "lesson_number" => 1, "part_number" => 1)
   end
 
   it "sets a student's progress" do
     Student.create(id: 3)
-    post "/progressions", progression: {student_id: 3, lesson_number: 3, part_number: 2}
+    post "/progressions", {student_id: 3, lesson_number: 3, part_number: 2}
 
     get "/students/3"
     json_response = JSON.parse(response.body)
