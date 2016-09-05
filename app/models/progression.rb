202d1579e3e6334e62b4623dedc60cc011083acd
MJ <mark.a@serviceseeking.com.au>
2016-09-06 00:40:12 +1000

    refactor to make next method more human readable
    * refactor out magic variables into constants
    * extract conditional method to document intent of guard clause

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -1,9 +1,18 @@
 class Progression < ActiveRecord::Base
+  NUMBER_OF_LESSONS = 100
+  PARTS_PER_LESSON = 3
+
   def next
-    return if part_number == 3 && lesson_number == 100
+    return if last_progression?
     next_part_number = part_number + 1
-    next_lesson_number = lesson_number + (next_part_number > 3 ? 1 : 0)
-    next_part_number = 1 if next_part_number > 3
+    next_lesson_number = lesson_number + (next_part_number > PARTS_PER_LESSON ? 1 : 0)
+    next_part_number = 1 if next_part_number > PARTS_PER_LESSON
     Progression.new(lesson_number: next_lesson_number, part_number: next_part_number, student_id: student_id)
+  end
+
+  private
+
+  def last_progression?
+    part_number == PARTS_PER_LESSON && lesson_number == NUMBER_OF_LESSONS
   end
 end
